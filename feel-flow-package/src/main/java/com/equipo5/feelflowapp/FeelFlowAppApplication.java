b83c6461a6922a1cfc2e5209cf8d6f2181bd420a
ianklebold <ianstgo@gmail.com>
2023-08-10 20:12:09 -0300

    Configuracion inicial

---
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/FeelFlowAppApplication.java
@@ -0,0 +1,13 @@
+package com.equipo5.feelflowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelFlowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelFlowAppApplication.class, args);
+	}
+
+}
---
+++ feel-flow-package/src/test/java/com/equipo5/feelflowapp/FeelFlowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo5.feelflowapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelFlowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
