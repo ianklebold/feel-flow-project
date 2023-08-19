5bfabaa62ced36a308ae781bea6047ddf59c7e69
ianklebold <ianstgo@gmail.com>
2023-08-19 15:27:02 -0300

    Anadiendo validate

---
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/validation/admin/ValidationAdminMessages.java
@@ -0,0 +1,18 @@
+package com.equipo5.feelflowapp.constants.validation.admin;
+
+public class ValidationAdminMessages {
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_NULL_NAME = "El nombre no puede ser nulo";
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_BLANK_NAME = "El nombre no puede ser vacio";
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_NULL_EMAIL = "El email no puede ser nulo";
+
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_BLANK_EMAIL = "El email no puede ser vacio";
+
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_WELL_FORMED_EMAIL = "No se cumple con el formato de email";
+
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_NULL_PASSWORD = "La password no puede ser nulo";
+
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_BLANK_PASSWORD = "La password no puede ser vacio";
+
+    public static final String VALIDATION_MESSAGE_ADMIN_NOT_WELL_FORMED_PASSWORD = "La password no cumple con el formato adecuado. Debe contener una letra mayúscula y al menos un número, con un minimo de 8 caracteres";
+
+}
--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/dto/users/admin/AdminDTO.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/dto/users/admin/AdminDTO.java
@@ -1,6 +1,8 @@
 package com.equipo5.feelflowapp.dto.users.admin;
 
+import com.equipo5.feelflowapp.constants.validation.admin.ValidationAdminMessages;
 import com.equipo5.feelflowapp.dto.enterprise.EnterpriseDTO;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.util.UUID;
@@ -12,8 +14,21 @@ import java.util.UUID;
 @Setter
 public class AdminDTO {
     private UUID uuid;
+
+    @NotBlank(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_BLANK_NAME)
+    @NotNull(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_NULL_NAME)
     private String name;
+
+    @NotNull(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_NULL_EMAIL)
+    @NotBlank(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_BLANK_EMAIL)
+    @Email(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_WELL_FORMED_EMAIL)
     private String email;
+
+    @NotNull(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_NULL_PASSWORD)
+    @NotBlank(message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_BLANK_PASSWORD)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\\\d).{8,99}$", message = ValidationAdminMessages.VALIDATION_MESSAGE_ADMIN_NOT_WELL_FORMED_PASSWORD)
     private String password;
+
+    @NotNull()
     private EnterpriseDTO enterpriseDTO;
 }
