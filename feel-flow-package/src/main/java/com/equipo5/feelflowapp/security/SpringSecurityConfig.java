8eb4d16eaa23c8c264d8009a166078db86aa7f23
ianklebold <ianstgo@gmail.com>
2023-08-20 16:16:11 -0300

    INICIO DE SESION - 000 - Agregando filtro JPA

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.equipo5.feelflowapp.security;
 
+import com.equipo5.feelflowapp.security.filters.JwtAutheticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -27,6 +32,10 @@ public class SpringSecurityConfig {
 
     @Bean //El objeto que se devuelve, se lo guarda en el contexto de spring para que otros componentes lo puedan usar
     SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
+
+        //Filtro de autenticacion --> Busqueda con JPA
+        httpSecurity.addFilter(new JwtAutheticationFilter(this.authenticationConfiguration.getAuthenticationManager()));
+
         httpSecurity.authorizeHttpRequests(
                         auth -> auth.requestMatchers("/api/v1/**").permitAll()
                                 .anyRequest()
---
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/filters/JwtAutheticationFilter.java
@@ -0,0 +1,72 @@
+package com.equipo5.feelflowapp.security.filters;
+
+import com.equipo5.feelflowapp.domain.users.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAutheticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final  AuthenticationManager authenticationManager;
+
+    public JwtAutheticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        User user = null;
+        String username = null;
+        String password = null;
+
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(),User.class);
+            username = user.getEmail();
+            password = user.getPassword();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,password);
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String username = ((org.springframework.security.core.userdetails.User) authResult.getPrincipal()).getUsername();
+        String originalInput = "algun_token_con_alguna_frase_secreta." + username;
+        String token = Base64.getEncoder().encodeToString(originalInput.getBytes());
+
+        response.addHeader("Authorization","Bearer" + token);
+        Map<String,Object> body = new HashMap<>();
+        body.put("token",token);
+        body.put("message",String.format("%s Haz iniciado sesion con exito",username));
+        body.put("username",username);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        Map<String,Object> body = new HashMap<>();
+        body.put("message","Error en la autenticacion username o password incorrecto");
+        body.put("error",failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(401);
+        response.setContentType("application/json");
+    }
+}
