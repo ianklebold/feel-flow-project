2cdb8802db39f65eb9ea4126060ad91b4656592c
Matias Snaider <mati.iva.41@gmail.com>
2023-08-21 18:45:11 -0300

    Inicio de sesión front - 005 - Adecuado de diseño y parte de la conexión con el backend

--- feel-flow-frontend/js/InicioSesion.js
+++ feel-flow-frontend/js/InicioSesion.js
@@ -1,18 +1,32 @@
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+const listUsers = async (email, password) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+
+  let isAuthenticated = false;
+
+  users.forEach((user) => {
+    if (user.email === email && user.username === password) {
+      isAuthenticated = true;
+    }
+  });
+
+  const messageContainer = document.getElementById("message-container");
+
+  if (!isAuthenticated) {
+    messageContainer.textContent = "Usuario o contraseña incorrectos.";
+  } else {
+    window.location.href = "../pages/sign_up.html";
+  }
+};
+
+document.getElementById("login-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Evita enviar el formulario
+
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  listUsers(email, password);
+});
