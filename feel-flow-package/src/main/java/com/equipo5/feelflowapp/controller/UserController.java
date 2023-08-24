d934f5dd9bf68b530bc82be6804c6ed82aad9965
ianklebold <ianstgo@gmail.com>
2023-08-24 18:33:58 -0300

    Actualizacion de datos de usuario - 002 - Terminando controller y agregando nueva excepcion

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/controller/UserController.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.equipo5.feelflowapp.controller;
 
 import com.equipo5.feelflowapp.dto.users.UserUpdateDTO;
+import com.equipo5.feelflowapp.exception.NotFoundException;
+import com.equipo5.feelflowapp.service.users.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -18,11 +22,18 @@ public class UserController {
 
     public static final String PATH_ID = "/{id}";
 
+    private final UserService userService;
+
     @PutMapping(PATH_ID)
-    public ResponseEntity updateUser(@PathVariable(value = "idUser")UUID idUser, @RequestBody UserUpdateDTO userUpdateDTO){
+    public ResponseEntity updateUser(@PathVariable(value = "idUser")UUID idUser, @RequestBody UserUpdateDTO userUpdateDTO) throws NotFoundException {
 
+        Optional<UserUpdateDTO> userUpdated = userService.updateUser(idUser,userUpdateDTO);
 
+        if (userUpdated.isEmpty()){
+            throw new NotFoundException();
+        }
 
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 
