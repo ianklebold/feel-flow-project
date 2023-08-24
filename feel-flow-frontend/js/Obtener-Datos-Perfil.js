4b0e82245dd4b6dc37e333570bb53aca96dfaf1c
Matias Snaider <mati.iva.41@gmail.com>
2023-08-24 18:40:08 -0300

    Perfil - front-end - Se teminó el backend de prueba

--- feel-flow-frontend/js/Obtener-Datos-Perfil.js
+++ feel-flow-frontend/js/Obtener-Datos-Perfil.js
@@ -1,24 +1,35 @@
-const listUsers = async () => {
+const listUsers = async (email) => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
   
     let tableBody = ``;
-    users.forEach((user, index) => {
-        tableBody += `
-        <li class="list-group-item border-0 ps-0 pt-0 text-sm"> <strong class="text-dark">Nombre Completo: </strong>${user.name}</li>
-        <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Email: </strong>${user.email}</li>
-        <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Empresa: </strong>${user.company.name}</li>
-        <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Equipo: </strong>${user.website}</li>
-        <li class="list-group-item border-0 ps-0 pb-0"> <strong class="text-dark">Rol: </strong>${user.username}</li>
-        `
+    let Nombre = ``;
+    let Rol = ``;
+    let Descripcion = ``;
+    users.forEach((user) => {
+        if (user.email === email) {
+            Nombre += `${user.name}`
+            Rol += `${user.username}`
+            Descripcion += `Hola`
+            tableBody += `
+            <li class="list-group-item border-0 ps-0 pt-0 text-sm"> <strong class="text-dark">Nombre Completo: </strong>${user.name}</li>
+            <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Email: </strong>${user.email}</li>
+            <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Empresa: </strong>${user.company.name}</li>
+            <li class="list-group-item border-0 ps-0 text-sm"> <strong class="text-dark">Equipo: </strong>${user.website}</li>
+            <li class="list-group-item border-0 ps-0 pb-0"> <strong class="text-dark">Rol: </strong>${user.username}</li>
+            `
+        }
+        
     });
-
+    document.getElementById("name").innerHTML = Nombre;
+    document.getElementById("rol").innerHTML = Rol;
+    document.getElementById("descripcion").innerHTML = Descripcion;
     document.getElementById("data").innerHTML = tableBody;
 
 };
 
 window.addEventListener("load", function (){
-    listUsers();
+    listUsers("Sincere@april.biz");
 })
 
 /*
