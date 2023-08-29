b792968c49d088eaa7c021f045e85ff41124f941
JorgeHadad <jorgemenegaz@gmail.com>
2023-08-29 20:56:27 -0300

    Add feel flow login page for automation

---
+++ feel-flow-testing/src/main/java/com/equipo5/carina/demo/gui/pages/desktop/FeelFlowLoginPage.java
@@ -0,0 +1,54 @@
+package com.equipo5.carina.demo.gui.pages.desktop;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+
+public class FeelFlowLoginPage extends AbstractPage {
+    
+    public FeelFlowLoginPage(WebDriver driver) {
+        super(driver);
+        setPageAbsoluteURL("https://www.feelflow.com/");
+    }
+    
+    @FindBy(xpath = "//*[@id=\"login-form\"]/div[3]/button")
+    private ExtendedWebElement signInButton;
+
+    @FindBy(xpath = "//*[@id=\"email\"]")
+    private ExtendedWebElement emailInput;
+
+    @FindBy(xpath = "//*[@id=\"password\"]")
+    private ExtendedWebElement passwordInput;
+
+    @FindBy(xpath = "//*[@id='submit-button']")
+    private ExtendedWebElement submitLogginButton;
+
+    @FindBy(xpath = "//*[@id=\"message-container\"]")
+    private ExtendedWebElement errorMessage;
+
+
+    public void open() {
+        this.openURL(this.pageURL);
+    }
+
+    
+    public void clickSignIn() {
+        signInButton.click();
+    }
+
+    public void enterParameters(String email, String password) {
+        emailInput.type(email);
+        passwordInput.type(password);
+    }
+
+    public void submitLoggin() {
+        submitLogginButton.click();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return errorMessage.isElementPresent();
+    }
+
+}
