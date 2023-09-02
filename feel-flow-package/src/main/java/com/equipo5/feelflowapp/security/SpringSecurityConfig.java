38df6edf38872d9c4827c66ab73fe76c54251dbd
ianklebold <ianstgo@gmail.com>
2023-09-02 18:17:06 -0300

    Cambio cors

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
@@ -47,7 +47,7 @@ public class SpringSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5500"));
+        config.setAllowedOriginPatterns(Arrays.asList("**"));
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
         config.setAllowCredentials(true);
