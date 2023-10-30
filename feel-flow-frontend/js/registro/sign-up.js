227903ac0834772372589cf658489392790c3780
Matias Snaider <mati.iva.41@gmail.com>
2023-10-29 21:39:57 -0300

    bug corregido

--- feel-flow-frontend/js/registro/sign-up.js
+++ feel-flow-frontend/js/registro/sign-up.js
@@ -5,6 +5,7 @@ async function Registrar(name, surname, username, password, enterpriseDTO) {
     username,
     password,
     enterpriseDTO
+
   };
 
   await fetch('http://localhost:8080/api/v1/admin', {
@@ -23,14 +24,17 @@ async function Registrar(name, surname, username, password, enterpriseDTO) {
         });
       }
     })
-    // .catch(error => {
-    //   if (error.errorData) {
+  // .catch(error => {
+  //   if (error.errorData) {
 
-    //   } else {
-    //     console.error('Error de red:', error);
-    //   }
-    // });
+  //   } else {
+  //     console.error('Error de red:', error);
+  //   }
+  // });
 }
+
+var ctl_pas = false;
+var ctl_usr = false;
 
 document.addEventListener('DOMContentLoaded', function () {
   const formulario = document.getElementById('registro-formulario');
@@ -64,8 +68,8 @@ document.addEventListener('DOMContentLoaded', function () {
   passwordInput.addEventListener('input', actualizarEstadoBoton);
   empresaInput.addEventListener('input', actualizarEstadoBoton);
 
-  var ctl_pas = false;
-  var ctl_usr = false;
+  var pas = false;
+  var usr = false;
 
   formulario.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -75,84 +79,194 @@ document.addEventListener('DOMContentLoaded', function () {
     const username = emailInput.value;
     const password = passwordInput.value;
     const enterpriseDTO = empresaInput.value;
+    const datos = {
+      name,
+      surname,
+      username,
+      password,
+      enterpriseDTO
 
-    Registrar(name, surname, username, password, enterpriseDTO)
-      .then(responseData => {
-        // La solicitud se completó con éxito, responseData contiene la respuesta
-        console.log('Registro exitoso:', responseData);
-        document.getElementById('flexCheckDefault')
+    };
+
+    if (!document.getElementById('flexCheckDefault').checked) {
+      document.getElementById('flexCheckDefault').classList.add('alert-danger');
+      document.getElementById('label-check').classList.add('text-danger');
+    } else {
+      document.getElementById('flexCheckDefault').classList.remove('alert-danger');
+      document.getElementById('label-check').classList.remove('text-danger');
+
+      fetch('http://localhost:8080/api/v1/admin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
       })
-      .catch(error => {
-        if (error.errorData) {
-          // El servidor respondió con un error 400, error contiene la respuesta
-          var pas = false;
-          var usr = false;
-          error.errorData.forEach(error => {
-            console.log(error)
-            for (const campo in error) {
-              var idinput = "in-" + campo;
-              var ctlinput = "clt-" + campo;
-              const mensaje = error[campo];
-              if (campo === "password") {
-                pas = true;
-                usr = false;
-              } else {
-                usr = true;
-                pas = false;
-              }
-              // Insertar el mensaje de error en el HTML, por ejemplo, en un elemento div con el id 'errores':
-              const divError = document.createElement('div');
-              divError.classList.add('invalid-tooltip', 'text-center');
-              divError.id = ctlinput;
-              divError.textContent = `${mensaje}`;
-              if (campo === "password") {
-                if (pas) {
+        .then(response => {
+          if (response.status === 201) {
+            window.location.href = '../pages/sign_in.html'
+          } else if (response.status === 400) {
+            if (error.errorData) {
+              var datosErroneos = error.errorData.length;
+
+              // El servidor respondió con un error 400, error contiene la respuesta
+              error.errorData.forEach(error => {
+                for (const campo in error) {
+                  var idinput = "in-" + campo;
+                  var ctlinput = "clt-" + campo;
+                  const mensaje = error[campo];
+
+                  if (datosErroneos < 2) {
+                    if (pas && ctl_pas) {
+                      document.getElementById('clt-password').remove();
+                      document.getElementById('in-password').classList.remove('incorrecto');
+                      ctl_pas = false;
+                      pas = false;
+                    }
+                    if (usr && ctl_usr) {
+                      document.getElementById('clt-username').remove();
+                      document.getElementById('in-username').classList.remove('incorrecto');
+                      ctl_usr = false;
+                      usr = false;
+                    }
+                  }
+
+                  if (campo === "password") {
+                    pas = true;
+                  } else {
+                    usr = true;
+                  }
+                  // Insertar el mensaje de error en el HTML, por ejemplo, en un elemento div con el id 'errores':
+                  const divError = document.createElement('div');
+                  divError.classList.add('invalid-tooltip', 'text-center');
+                  divError.id = ctlinput;
+                  divError.textContent = `${mensaje}`;
+
+                  if (campo === "password") {
+                    if (!ctl_pas) {
+                      document.getElementById(`${campo}`).appendChild(divError);
+                      document.getElementById(`${idinput}`).classList.add('incorrecto');
+                      ctl_pas = true;
+                    }
+                  }
+
+                  if (campo === "username") {
+                    if (!ctl_usr) {
+                      document.getElementById(`${campo}`).appendChild(divError);
+                      document.getElementById(`${idinput}`).classList.add('incorrecto');
+                      ctl_usr = true;
+                    }
+                  }
+
+                  // Controlar checkbox
+                  if (!document.getElementById('flexCheckDefault').checked) {
+                    document.getElementById('flexCheckDefault').classList.add('alert-danger');
+                    document.getElementById('label-check').classList.add('text-danger');
+                  } else {
+                    document.getElementById('flexCheckDefault').classList.remove('alert-danger');
+                    document.getElementById('label-check').classList.remove('text-danger');
+                  }
+
+                }
+              });
+
+            } else {
+              // Otro tipo de error, como error de red
+              console.error('Error de red u otro error:', error);
+            }
+          }
+        })
+/*
+      Registrar(name, surname, username, password, enterpriseDTO)
+        .then(responseData => {
+          // La solicitud se completó con éxito, responseData contiene la respuesta
+          console.log('Registro exitoso:', responseData);
+          document.getElementById('flexCheckDefault')
+          // Controlar checkbox
+
+        })
+        .catch(error => {
+          if (error.errorData) {
+            console.log(pas + ' ' + ctl_pas);
+            console.log(usr + ' ' + ctl_usr);
+
+            var datosErroneos = error.errorData.length;
+
+            // El servidor respondió con un error 400, error contiene la respuesta
+
+            console.log(error.errorData.length)
+            error.errorData.forEach(error => {
+              console.log(error)
+              for (const campo in error) {
+                var idinput = "in-" + campo;
+                var ctlinput = "clt-" + campo;
+                const mensaje = error[campo];
+                console.log(campo)
+
+                if (datosErroneos < 2) {
+                  if (pas && ctl_pas) {
+                    document.getElementById('clt-password').remove();
+                    document.getElementById('in-password').classList.remove('incorrecto');
+                    ctl_pas = false;
+                    pas = false;
+                    console.log("remove pass")
+                  }
+                  if (usr && ctl_usr) {
+                    document.getElementById('clt-username').remove();
+                    document.getElementById('in-username').classList.remove('incorrecto');
+                    ctl_usr = false;
+                    usr = false;
+                    console.log("remove user")
+                  }
+                }
+
+                if (campo === "password") {
+                  pas = true;
+                  console.log(pas)
+                } else {
+                  usr = true;
+                  console.log(usr)
+                }
+                // Insertar el mensaje de error en el HTML, por ejemplo, en un elemento div con el id 'errores':
+                const divError = document.createElement('div');
+                divError.classList.add('invalid-tooltip', 'text-center');
+                divError.id = ctlinput;
+                divError.textContent = `${mensaje}`;
+
+                if (campo === "password") {
                   if (!ctl_pas) {
                     document.getElementById(`${campo}`).appendChild(divError);
                     document.getElementById(`${idinput}`).classList.add('incorrecto');
                     ctl_pas = true;
                   }
-                } else {
-                  if (ctl_pas) {
-                    document.getElementById(`${ctlinput}`).remove();
-                    document.getElementById(`${idinput}`).classList.remove('incorrecto');
-                    ctl_pas = false;
-                    console.log("remove pass")
-                  }
                 }
-              } else {
-                if (usr) {
+
+                if (campo === "username") {
                   if (!ctl_usr) {
                     document.getElementById(`${campo}`).appendChild(divError);
                     document.getElementById(`${idinput}`).classList.add('incorrecto');
                     ctl_usr = true;
-                  } else {
-                    if (ctl_usr) {
-                      document.getElementById(`${ctlinput}`).remove();
-                      document.getElementById(`${idinput}`).classList.remove('incorrecto');
-                      ctl_usr = false;
-                      console.log("remove user")
-                    }
                   }
                 }
-              }
 
-              // Controlar checkbox
-              if (!document.getElementById('flexCheckDefault').checked) {
-                document.getElementById('flexCheckDefault').classList.add('alert-danger');
-                document.getElementById('label-check').classList.add('text-danger');
-              } else {
-                document.getElementById('flexCheckDefault').classList.remove('alert-danger');
-                document.getElementById('label-check').classList.remove('text-danger');
-              }
+                // Controlar checkbox
+                if (!document.getElementById('flexCheckDefault').checked) {
+                  document.getElementById('flexCheckDefault').classList.add('alert-danger');
+                  document.getElementById('label-check').classList.add('text-danger');
+                } else {
+                  document.getElementById('flexCheckDefault').classList.remove('alert-danger');
+                  document.getElementById('label-check').classList.remove('text-danger');
+                }
 
-            }
-          });
-        } else {
-          // Otro tipo de error, como error de red
-          console.error('Error de red u otro error:', error);
-        }
-      });
+              }
+            });
+
+          } else {
+            // Otro tipo de error, como error de red
+            console.error('Error de red u otro error:', error);
+          }
+        });*/
+    }
   });
 
   // Llama a esta función para configurar el estado inicial del botón
