bba3b7306057f281c272ac47e2738ced9382b84d
Matias Snaider <mati.iva.41@gmail.com>
2023-10-31 01:38:07 -0300

    anda los links de invite

--- feel-flow-frontend/js/controller/aside.js
+++ feel-flow-frontend/js/controller/aside.js
@@ -2,6 +2,7 @@ import { GetUser } from "../functions/GetPerfil.js";
 
 const idLocation = localStorage.getItem('idLocation');
 const token = localStorage.getItem('Token');
+console.log(token)
 
 const titulo_pagina = document.querySelector('title');
 const pagina = titulo_pagina.textContent;
