19eee63a4c1d8efadc66825a0d956aa1d9b161a8
ianklebold <ianstgo@gmail.com>
2023-10-31 02:21:15 -0300

    Fix security

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
@@ -49,7 +49,7 @@ public class SpringSecurityConfig {
     SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/api/v1/user/**").hasAnyAuthority("TEAM_LEADER","ADMIN")
+                                .requestMatchers("/api/v1/user/**").hasAnyAuthority("TEAM_LEADER","ADMIN","REGULAR_USER")
                                 .requestMatchers(HttpMethod.POST,"/api/v1/admin/**").permitAll()
                                 .requestMatchers("/api/v1/admin/**").hasAnyAuthority("TEAM_LEADER","ADMIN")
                                 .requestMatchers(HttpMethod.PUT,"/api/v1/team/{idTeam}").hasAnyAuthority("TEAM_LEADER","ADMIN")
