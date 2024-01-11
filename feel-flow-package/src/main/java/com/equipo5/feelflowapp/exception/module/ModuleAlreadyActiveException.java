e49be4562a8f3147e0a71207196c104ffb727e29
ianklebold <ianstgo@gmail.com>
2024-01-11 16:55:46 -0300

    GlobalExceptionHandler creado

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/exception/module/ModuleAlreadyActiveException.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/exception/module/ModuleAlreadyActiveException.java
@@ -3,8 +3,8 @@ package com.equipo5.feelflowapp.exception.module;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invitation was approved")
-public class ModuleAlreadyActiveException extends IllegalArgumentException{
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Already a module is active")
+public class ModuleAlreadyActiveException extends ModuleException{
 
     public ModuleAlreadyActiveException() {
     }
---
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/exception/module/ModuleException.java
@@ -0,0 +1,22 @@
+package com.equipo5.feelflowapp.exception.module;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Module Exception error")
+public class ModuleException extends IllegalArgumentException{
+    public ModuleException() {
+    }
+
+    public ModuleException(String s) {
+        super(s);
+    }
+
+    public ModuleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ModuleException(Throwable cause) {
+        super(cause);
+    }
+}
