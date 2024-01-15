b4e692b05ffd68678a3e4dc5d05601fda445f3c3
ianklebold <ianstgo@gmail.com>
2024-01-14 21:53:10 -0300

    Agregando nuevo puerto para conexion desde front

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/security/SpringSecurityConfig.java
@@ -71,7 +71,9 @@ public class SpringSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:8000", "http://127.0.0.1:5500", "http://127.0.0.1:3000"));
+        config.setAllowedOrigins(
+                Arrays.asList("http://127.0.0.1:8000", "http://127.0.0.1:5500", "http://127.0.0.1:3000","http://127.0.0.1:5173")
+        );
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
