b781c35ece75c99f90338b8791da7e79f628aa15
Matias Snaider <mati.iva.41@gmail.com>
2024-01-30 19:51:34 -0300

    resguardo de lo contestado

--- feel-flow-frontend/js/encuesta.js
+++ feel-flow-frontend/js/encuesta.js
@@ -11,9 +11,9 @@ const autoridad = payloadObjeto.authorities;
 const autoridad_rol = JSON.parse(autoridad);
 const rol = autoridad_rol[0].authority;
 
+const encuesta = []
 
 function Preguntas() {
-    console.log("hola");
     ObtenerPreguntas(token)
         .then(data => {
             var form = document.createElement('form');
@@ -28,38 +28,42 @@ function Preguntas() {
                 pregunta.textContent = data[i];
                 pregunta.setAttribute("id", 'pregunta-' + i);
 
-            
+
                 preguntaDiv.appendChild(pregunta);
                 form.appendChild(preguntaDiv);
 
                 var divisor = document.getElementById("preguntas");
                 divisor.insertAdjacentElement("beforeend", form);
 
-                console.log(data[i]);
+                // console.log(data[i]);
             }
 
             return data.length;
         })
         .catch(error => {
-            console.error("Error al obtener las preguntas");
+            console.error("Error al obtener las preguntas: " + error);
             return 0;
         });
 }
 
 
 function Respuestas() {
-    console.log("hola");
     ObtenerRespuestas(token)
         .then(data => {
-            for (var i = 0; i < data.length; i++) {
-                for (var j = 0; j < data[i].length; j++) {
+            for (var i = 0; i < data.length; i++) { // Recorre el grupo de respuestas que corresponde a la pregunta | de 1 a 12
+                for (var j = 0; j < data[i].length; j++) { // Recorre cada respuesta de cada pregunta | de 1 a 5
                     var respuesta = document.createElement('label');
+
                     respuesta.classList.add('ps-4', 'ms-2', 'my-4', 'text-uppercase', 'text-xs', 'font-weight-bolder', 'opacity-6');
 
                     var checkbox = document.createElement('input');
                     checkbox.setAttribute('type', 'radio');
                     checkbox.setAttribute('name', 'respuestaRadio-' + i);
                     checkbox.setAttribute('value', data[i][j]);
+                    if (encuesta[i] == j) {
+                        console.log(data[i][j])
+                        checkbox.setAttribute('checked', true)
+                    }
 
                     respuesta.appendChild(checkbox);
                     respuesta.appendChild(document.createTextNode(data[i][j]));
@@ -67,16 +71,62 @@ function Respuestas() {
                     var divisor = document.getElementById("pregunta-" + i.toString());
                     divisor.insertAdjacentElement("beforeend", respuesta);
 
-                    console.log(data[i][j]);
+                    // console.log(data[i][j]);
                 }
             }
         })
         .catch(error => {
-            console.error("Error al obtener las respuestas");
+            console.error("Error al obtener las respuestas" + error);
             return 0;
         });
 }
 
+function toNumber(number) {
+    let nro
+    switch (number) {
+        case 'I am Ian':
+            nro = 0;
+            break;
+        case 'UNO':
+            nro = 1;
+            break;
+        case 'DOS':
+            nro = 2;
+            break;
+        case 'TRES':
+            nro = 3;
+            break;
+        case 'CUATRO':
+            nro = 4;
+            break;
+        case 'CINCO':
+            nro = 5;
+            break;
+    }
+    return nro
+}
+
+function EstadoEncuesta() {
+    return ObtenerEncuestas(token)
+        .then(data => {
+            let activity = data[0].activityList;
+            for (let i = 0; i < activity.length; i++) {
+                encuesta.push(toNumber(data[0].activityList[i].answer));
+            }
+        })
+        .catch(error => {
+            console.error("Error al obtener las encuestas: " + error);
+            throw error; // Lanza el error nuevamente para que sea manejado por el siguiente `catch`
+        });
+}
+
+function MostrarRespuestas(respuestas) {
+
+}
+
+function MostrarPantalla() {
+    EstadoEncuesta()
+}
 
 // function Preguntas() {
 //     console.log("hola")
@@ -108,13 +158,13 @@ function Respuestas() {
 //     console.log("hola")
 //     ObtenerRespuestas(token)
 //         .then(data => {
-            
+
 //             for (var i = 0; i < data.length; i++) {
 //                 for (var j = 0; j < data[i].length; j++) {
 //                     var respuesta = document.createElement('p');
 //                     respuesta.classList.add('ps-4', 'ms-2', 'my-4', 'text-uppercase', 'text-xs', 'font-weight-bolder', 'opacity-6');
 //                     respuesta.textContent = data[i][j];
-                    
+
 //                     var divisor = document.getElementById(i.toString());
 //                     divisor.insertAdjacentElement("beforeend", respuesta);
 
@@ -130,3 +180,4 @@ function Respuestas() {
 
 Preguntas(token);
 Respuestas(token);
+MostrarPantalla();
