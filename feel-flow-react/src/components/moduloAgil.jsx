12108f90d59c5b75adb2331eebed82cf5b837e30
Facundo Bordes <facundonicolasbordes@gmail.com>
2025-01-06 14:50:43 -0300

    ajusto margenes v2

--- feel-flow-react/src/components/moduloAgil.jsx
+++ feel-flow-react/src/components/moduloAgil.jsx
@@ -60,7 +60,7 @@ const Modulo = ({
 
                                 )}
                             </div>
-                            <div className="relative flex flex-col items-center text-white w-full mt-4">
+                            <div className="relative flex flex-col items-center text-white w-full mt-5">
                                 {descripcionModulo && (
                                     <p className="text-center p-4">{descripcionModulo}</p>
                                 )}
