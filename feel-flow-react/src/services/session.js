29e6c53e9ff7cdd38244166e3c652b45b865c88e
Matias Snaider <mati.iva.41@gmail.com>
2025-01-08 20:33:38 -0300

    session storage

--- feel-flow-react/src/services/session.js
+++ feel-flow-react/src/services/session.js
@@ -1,41 +1,33 @@
-// Guardar datos en localStorage
+const saveUserData = (token) => {
+    const payload = token.split('.')[1];
+    const decodedPayload = JSON.parse(atob(payload)); 
+
+    sessionStorage.setItem("userID", decodedPayload.id);
+    sessionStorage.setItem("isAdmin", decodedPayload.isAdmin);
+    sessionStorage.setItem("authority", JSON.parse(decodedPayload.authorities)[0].authority);
+}
+
 export const saveAuthData = (token, username) => {
     sessionStorage.setItem("token", token);
     sessionStorage.setItem("username", username);
+    saveUserData(token);
 };
 
-// Obtener datos de localStorage
 export const getAuthData = () => {
     const token = sessionStorage.getItem("token");
     const username = sessionStorage.getItem("username");
     return { token, username };
 };
 
-// Limpiar datos de localStorage (para logout)
+export const getUserData = () => {
+    const username = sessionStorage.getItem("username");
+    const userID = sessionStorage.getItem("userID");
+    const isAdmin = sessionStorage.getItem("isAdmin");
+    const authority = sessionStorage.getItem("authority");
+    return { username, userID, isAdmin, authority }
+}
+
 export const clearAuthData = () => {
     sessionStorage.removeItem("token");
     sessionStorage.removeItem("username");
 };
-
-export const decodeJWT = () => {
-    try {
-        const { token, username } = getAuthData();
-        const payload = token.split('.')[1];
-        const decodedPayload = JSON.parse(atob(payload)); 
-        return decodedPayload;
-    } catch (error) {
-        console.error("Error al decodificar el token:", error);
-        throw new Error("Token inválido.");
-    }
-};
-
-
-// En la consola del navegador
-console.log("MOSTRANDO EL TOKEN e ID EN LOCALSTORAGE");
-console.log(localStorage.getItem('token'));  // Debe mostrar el token almacenado
-console.log(localStorage.getItem('userId'));
-
-// En la consola del navegador
-console.log("MOSTRANDO EL TOKEN e ID EN sessionStorage");
-console.log(sessionStorage.getItem('token'));  // Debe mostrar el token almacenado
-console.log(sessionStorage.getItem('username'));
