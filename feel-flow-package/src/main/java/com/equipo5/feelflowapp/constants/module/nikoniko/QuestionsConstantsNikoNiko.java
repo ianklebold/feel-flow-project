434a3261b50feff2db5591c3c27af26771d41637
ianklebold <ianstgo@gmail.com>
2025-03-28 19:57:33 -0300

    Agregando constantes de respuestas para niko niko

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/module/nikoniko/QuestionsConstantsNikoNiko.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/module/nikoniko/QuestionsConstantsNikoNiko.java
@@ -2,7 +2,7 @@ package com.equipo5.feelflowapp.constants.module.nikoniko;
 
 public class QuestionsConstantsNikoNiko {
 
-    public static final String ANSWERS_1_POOL_NIKO_NIKO = "Como te sientes el dia de hoy?";
+    public static final String QUESTIONS_1_POOL_NIKO_NIKO = "Como te sientes el dia de hoy?";
 
-    public static final String ANSWERS_2_POOL_NIKO_NIKO = "Como te sientes luego de la jornada laboral?";
+    public static final String QUESTIONS_2_POOL_NIKO_NIKO = "Como te sientes luego de la jornada laboral?";
 }
---
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/module/nikoniko/ResponseConstantsNikoNiko.java
@@ -0,0 +1,9 @@
+package com.equipo5.feelflowapp.constants.module.nikoniko;
+
+public class ResponseConstantsNikoNiko {
+    public static final String ANSWERS_1_POOL_NIKO_NIKO = "Muy Bien";
+    public static final String ANSWERS_2_POOL_NIKO_NIKO = "Bien";
+    public static final String ANSWERS_3_POOL_NIKO_NIKO = "Normal";
+    public static final String ANSWERS_4_POOL_NIKO_NIKO = "Mal";
+    public static final String ANSWERS_5_POOL_NIKO_NIKO = "Muy Mal";
+}
