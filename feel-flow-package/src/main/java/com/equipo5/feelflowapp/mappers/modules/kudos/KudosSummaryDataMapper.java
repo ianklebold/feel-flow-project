9e5e2d0632b6fd4df1aa565c7842aad0dadfc305
ianklebold <ianstgo@gmail.com>
2025-04-02 15:08:51 -0300

    Correccion en mapper

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/mappers/modules/kudos/KudosSummaryDataMapper.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/mappers/modules/kudos/KudosSummaryDataMapper.java
@@ -3,18 +3,16 @@ package com.equipo5.feelflowapp.mappers.modules.kudos;
 import com.equipo5.feelflowapp.dto.dashboard.kudos.KudosSummaryData;
 import com.equipo5.feelflowapp.dto.tablebadge.CountBadgeAwardedDto;
 import com.equipo5.feelflowapp.dto.tablebadge.TableBadgeAwardedDto;
-import com.equipo5.feelflowapp.service.utils.dateservice.DateUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
-import java.sql.Timestamp;
 
 @Mapper
 public interface KudosSummaryDataMapper {
 
     @Mapping(source = "username", target = "username")
-    @Mapping(source = "highlight", target = "manosAmigasBadge", qualifiedByName = "setHighLightDependsCountOfManosAmigasBadge")
+    @Mapping(source = "manosAmigasBadge", target = "highlight", qualifiedByName = "setHighLightDependsCountOfManosAmigasBadge")
     KudosSummaryData tableBadgeAwardedDtoToKudosSummaryData(TableBadgeAwardedDto tableBadgeAwardedDto);
 
     @Named("setHighLightDependsCountOfManosAmigasBadge")
