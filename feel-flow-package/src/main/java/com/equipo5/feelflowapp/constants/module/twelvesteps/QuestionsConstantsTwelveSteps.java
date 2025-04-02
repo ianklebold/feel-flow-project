920066bdcd2c8ce65ad7f2b0f710c45fd8af1d7e
ianklebold <ianstgo@gmail.com>
2025-04-02 15:49:22 -0300

    Cambios en el pool de preguntas

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/module/twelvesteps/QuestionsConstantsTwelveSteps.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/module/twelvesteps/QuestionsConstantsTwelveSteps.java
@@ -36,119 +36,119 @@ public class QuestionsConstantsTwelveSteps {
 
     public static final String[] ANSWERS_1_POOL_CLASSIC = {
 
-                    "1. Muy Poco Expresado",
-                    "2. Poco Expresado",
-                    "3. Neutral",
-                    "4. Expresado",
-                    "5. Muy Expresado"
+                    "1. Totalmente claro y entiendo mi contribución",
+                    "2. Bastante claro, aunque a veces tengo dudas",
+                    "3. Algo confuso, no siempre lo entiendo",
+                    "4. Poco claro, rara vez sé cómo contribuyo",
+                    "5. Completamente confuso, no entiendo el propósito del equipo"
             };
 
 
     public static final String[] ANSWERS_2_POOL_CLASSIC = {
 
-            "1. Muy Poco Facilitado",
-            "2. Poco Facilitado",
-            "3. Neutral",
-            "4. Facilitado",
-            "5. Muy Facilitado"
+            "1. Siempre puedo tomar decisiones libremente",
+            "2. Casi siempre tengo autonomía suficiente",
+            "3. A veces puedo decidir, pero no siempre",
+            "4. Raramente puedo tomar decisiones por mi cuenta",
+            "5. Nunca tengo autonomía"
     };
 
 
     public static final String[] ANSWERS_3_POOL_CLASSIC = {
 
-            "1. Muy Poca Promoción",
-            "2. Poca Promoción",
-            "3. Neutral",
-            "4. Buena Promoción",
-            "5. Muy Buena Promoción"
+            "1. Sí, siempre me siento apoyado para mejorar.",
+            "2. En general, hay apoyo suficiente",
+            "3. A veces siento que me faltan oportunidades",
+            "4. Rara vez recibo apoyo para mejorar.",
+            "5. Nunca siento que hay interés en mi crecimiento"
     };
 
 
     public static final String[] ANSWERS_4_POOL_CLASSIC = {
 
-            "1. Muy Poco Fomentado",
-            "2. Poco Fomentado",
-            "3. Neutral",
-            "4. Bien Fomentado",
-            "5. Muy Expresado"
+            "1. Sí, siempre siento una conexión fuerte y apoyo mutuo",
+            "2. Generalmente, hay un buen nivel de conexión",
+            "3. A veces hay conexión, pero no siempre",
+            "4. Raramente siento una conexión real en el equipo",
+            "5. Nunca siento que haya conexión o apoyo"
     };
 
 
     public static final String[] ANSWERS_5_POOL_CLASSIC = {
 
-            "1. Muy Poco Expresado",
-            "2. Poco Expresado",
-            "3. Neutral",
-            "4. Expresado",
-            "5. Muy Bien Fomentado"
+            "1. Siempre se reconocen y valoran",
+            "2. Generalmente se reconocen, aunque no siempre.",
+            "3. A veces se reconocen, pero podría ser mejor",
+            "4. Rara vez siento reconocimiento",
+            "5. Nunca me siento reconocido."
     };
 
 
     public static final String[] ANSWERS_6_POOL_CLASSIC = {
 
-            "1. Muy Poca Promoción",
-            "2. Poca Promoción",
-            "3. Neutral",
-            "4. Buena Promoción",
-            "5. Muy Buena Promoción"
+            "1. Siempre siento que mi trabajo tiene impacto.",
+            "2. Generalmente siento que estoy haciendo una diferencia.",
+            "3. A veces siento que mi trabajo impacta, pero no siempre.",
+            "4. Rara vez siento que mi trabajo importa.",
+            "5. Nunca siento que mi trabajo tenga impacto."
     };
 
 
     public static final String[] ANSWERS_7_POOL_CLASSIC = {
 
-            "1. Muy Poco Fomento",
-            "2. Poco Fomento",
-            "3. Neutral",
-            "4. Buen Fomento",
-            "5. Muy Buen Fomento"
+            "1. Siempre me siento seguro para expresarme",
+            "2. Generalmente puedo hablar con confianza",
+            "3. A veces puedo expresarme, pero con reservas",
+            "4. Raramente me siento cómodo expresándome",
+            "5. Nunca me siento seguro para compartir mis pensamientos"
     };
 
 
     public static final String[] ANSWERS_8_POOL_CLASSIC = {
 
-            "1. Muy Poco Estímulo",
-            "2. Poco Estímulo",
-            "3. Neutral",
-            "4. Buen Estímulo",
-            "5. Muy Buen Estímulo"
+            "1. Siempre hay momentos divertidos y relajantes",
+            "2. Generalmente hay oportunidades para relajarse",
+            "3. A veces hay momentos de diversión, pero no muchos",
+            "4. Rara vez hay espacio para divertirse",
+            "5. Nunca hay espacio para diversión o relajación"
     };
 
 
     public static final String[] ANSWERS_9_POOL_CLASSIC = {
 
-            "1. Muy Poco Apoyo",
-            "2. Poco Apoyo",
-            "3. Neutral",
-            "4. Buen Apoyo",
-            "5. Muy Buen Apoyo"
+            "1. Siempre me siento apoyado en mi desarrollo",
+            "2. Generalmente me siento apoyado",
+            "3. A veces siento apoyo, pero podría ser mejor",
+            "4. Rara vez siento apoyo para crecer",
+            "5. Nunca siento que el equipo se preocupe por mi desarrollo"
     };
 
 
     public static final String[] ANSWERS_10_POOL_CLASSIC = {
 
-            "1. Muy Poca Facilitación",
-            "2. Poca Facilitación",
-            "3. Neutral",
-            "4. Buena Facilitación",
-            "5. Muy Buena Facilitación"
+            "1. Siempre se fomenta la atención plena",
+            "2. Generalmente hay un buen ambiente para concentrarse",
+            "3. A veces se fomenta, pero no siempre",
+            "4. Rara vez se promueve la atención plena",
+            "5. Nunca se fomenta la concentración"
     };
 
     public static final String[] ANSWERS_11_POOL_CLASSIC = {
 
-            "1. Muy Poco Fomento",
-            "2. Poco Fomento",
-            "3. Neutral",
-            "4. Buen Fomento",
-            "5. Muy Buen Fomento"
+            "1. Siempre se practica y se siente genuina",
+            "2. Generalmente se expresa gratitud",
+            "3. A veces se expresa, pero no mucho",
+            "4. Rara vez siento gratitud en el equipo",
+            "5. Nunca se practica la gratitud"
     };
 
     public static final String[] ANSWERS_12_POOL_CLASSIC = {
 
-            "1. Muy Poca Contribución",
-            "2. Poca Contribución",
-            "3. Neutral",
-            "4. Buena Contribución",
-            "5. Muy Buena Contribución"
+            "1. Totalmente alineados",
+            "2. Bastante alineados",
+            "3. Algo alineados",
+            "4. Poco alineados",
+            "5. Completamente desalineados"
     };
 
 }
