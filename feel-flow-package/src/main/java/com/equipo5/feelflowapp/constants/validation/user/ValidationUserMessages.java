90cb8f75112f2fc24f298c2d6ba54a3fa60bd7fc
ianklebold <ianstgo@gmail.com>
2025-04-06 22:40:52 -0300

    Fix en DTO de user para actualizar

--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/validation/user/ValidationUserMessages.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/constants/validation/user/ValidationUserMessages.java
@@ -15,6 +15,9 @@ public class ValidationUserMessages {
     public static final String VALIDATION_MESSAGE_NOT_NULL_COUNTRY = "El pais no puede ser nulo";
     public static final String VALIDATION_MESSAGE_NOT_BLANK_CUNTRY = "El pais no puede ser vacio";
 
+    public static final String VALIDATION_MESSAGE_NOT_NULL_DESCRIPTION = "La descripcion no puede ser nulo";
+    public static final String VALIDATION_MESSAGE_NOT_BLANK_DESCRIPTION = "La descripcion no puede ser vacio";
+
     public static final String VALIDATION_PHONE_NUMBER = "El numero de telefono no es valido";
 
 }
--- feel-flow-package/src/main/java/com/equipo5/feelflowapp/dto/users/UserUpdateDTO.java
+++ feel-flow-package/src/main/java/com/equipo5/feelflowapp/dto/users/UserUpdateDTO.java
@@ -35,7 +35,7 @@ public class UserUpdateDTO {
     @Pattern(regexp = "^\\+[1-9]\\d{1,14}$", message = ValidationUserMessages.VALIDATION_PHONE_NUMBER)
     private String phoneNumber;
 
-    @NotNull(message = ValidationUserMessages.VALIDATION_MESSAGE_NOT_NULL_COUNTRY)
-    @NotBlank(message = ValidationUserMessages.VALIDATION_MESSAGE_NOT_BLANK_CUNTRY)
+    @NotNull(message = ValidationUserMessages.VALIDATION_MESSAGE_NOT_NULL_DESCRIPTION)
+    @NotBlank(message = ValidationUserMessages.VALIDATION_MESSAGE_NOT_BLANK_DESCRIPTION)
     private String description;
 }
